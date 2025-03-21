cee9132ffb29c9d1c27d46321198700e988e3bb1
Piotr Mucha <piotr.mucha@volt.io>
2025-03-21 14:17:43 +0100

    delta

--- main.py
+++ main.py
@@ -6,8 +6,10 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
-
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
 
     while True:
         for event in pygame.event.get():
@@ -15,6 +17,7 @@ def main():
                 return
         screen.fill(color="black")
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
